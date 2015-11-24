14caeda1c4d3f8db3f410cce50539b8788e76cc0
Fumba Chibaka <fumba.me@gmail.com>
2015-11-24 17:02:06 -0500

    Footer background included

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -28,7 +28,7 @@ jQuery(document).ready(function() {
     $('.top-content').backstretch("static/img/backgrounds/1.jpg");
     $('.how-it-works-container').backstretch("static/img/backgrounds/1.jpg");
     $('.call-to-action-container').backstretch("static/img/backgrounds/1.jpg");
-    $('.testimonials-container').backstretch("static/img/backgrounds/1.jpg");
+    $('.footer-container').backstretch("static/img/backgrounds/1.jpg");
     
     $('#top-navbar-1').on('shown.bs.collapse', function(){
     	$('.top-content').backstretch("resize");
